77dd92067f24791c25b7f34e3af55f12bc546ea5
Rohan Kushwaha <109597805+Krohan591@users.noreply.github.com>
2025-08-27 03:25:29 +0530

    Refactor PINN class and update hidden layers
    Refactor PINN class to accept parameters A, B, C directly in the constructor. Update hidden layers structure and remove the build_hidden_layers method.

--- model.py
+++ model.py
@@ -1,43 +1,41 @@
-import tensorflow as tf
+# coding: utf-8
+import tqdm as tqdm
 from keras.saving import register_keras_serializable
+from tensorflow.keras import layers, models, regularizers
+import tensorflow as tf
 
 @register_keras_serializable()
-class PINN(tf.keras.Model):
-    def __init__(self, hidden_layers=None, dropout_rate=0.6, **kwargs):
-        super(PINN, self).__init__(**kwargs)
 
+class PINN(tf.keras.Model):
+    def __init__(self,A,B,C,**kwargs):
+        super(PINN,self).__init__(**kwargs)
+        self.A = A
+        self.B = B
+        self.C = C
         self.initializer = tf.keras.initializers.HeNormal()
         self.activation_function = tf.keras.activations.relu
-        self.hidden = self.build_hidden_layers(hidden_layers, dropout_rate)
+        self.hidden = tf.keras.Sequential([
+            tf.keras.layers.Dense(32, activation='relu'),
+            # tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dropout(0.1),
 
+            tf.keras.layers.Dense(256, activation=self.activation_function, kernel_initializer=self.initializer),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dropout(0.4),
+
+            tf.keras.layers.Dense(256, activation=self.activation_function, kernel_initializer=self.initializer),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dropout(0.4),
+            
+            tf.keras.layers.Dense(32, activation='relu'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dense(1,activation='sigmoid')  # Output: K (fracture toughness)
+            ])
+        
         # Trainable physics constants
-        self.A = tf.Variable(18.0, dtype=tf.float32, trainable=True)
-        self.B = tf.Variable(18.0, dtype=tf.float32, trainable=True)
-        self.C = tf.Variable(2.0, dtype=tf.float32, trainable=True)
-
-    def build_hidden_layers(self, hidden_layers, dropout_rate):
-        if hidden_layers is None:
-            hidden_layers = [128, 64]
-
-        layers = []
-        for units in hidden_layers:
-            layers.append(tf.keras.layers.Dense(units, activation=self.activation_function, kernel_initializer=self.initializer))
-            layers.append(tf.keras.layers.BatchNormalization())
-            layers.append(tf.keras.layers.Dropout(dropout_rate))
-
-        layers.append(tf.keras.layers.Dense(1, activation='sigmoid'))  # Output layer
-        return tf.keras.Sequential(layers)
+        self.A = tf.Variable(self.A, dtype=tf.float32, trainable=True)
+        self.B = tf.Variable(self.B, dtype=tf.float32, trainable=True)
+        self.C = tf.Variable(self.C, dtype=tf.float32, trainable=True)
 
     def call(self, x):
         return self.hidden(x)
-    
-    def get_config(self):
-        config = super().get_config()
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
-
-def create_pinn_model(hidden_layers=None, dropout_rate=0.6):
-    return PINN(hidden_layers=hidden_layers, dropout_rate=dropout_rate)
